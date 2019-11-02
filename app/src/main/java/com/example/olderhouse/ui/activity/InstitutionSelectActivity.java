c46aa0eee16fe021fb35a86559d287f078776e5c
rfa <1017201184@qq.com>
2019-11-02 17:16:30 +0800

    添加了下拉菜单的控件

--- app/src/main/java/com/example/olderhouse/ui/activity/InstitutionSelectActivity.java
+++ app/src/main/java/com/example/olderhouse/ui/activity/InstitutionSelectActivity.java
@@ -2,14 +2,147 @@ package com.example.olderhouse.ui.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.TypedValue;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.AdapterView;
+import android.widget.GridView;
+import android.widget.ListView;
+import android.widget.TextView;
 
 import com.example.olderhouse.R;
+import com.example.olderhouse.ui.adapter.ConstellationAdapter;
+import com.example.olderhouse.ui.adapter.GirdDropDownAdapter;
+import com.example.olderhouse.ui.adapter.ListDropDownAdapter;
+import com.yyydjk.library.DropDownMenu;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class InstitutionSelectActivity extends AppCompatActivity {
+
+    DropDownMenu mDropDownMenu;
+    private String headers[] = {"城市", "年龄", "性别", "星座"};
+    private List<View> popupViews = new ArrayList<>();
+
+    private GirdDropDownAdapter cityAdapter;
+    private ListDropDownAdapter ageAdapter;
+    private ListDropDownAdapter sexAdapter;
+    private ConstellationAdapter constellationAdapter;
+
+    private String citys[] = {"不限", "武汉", "北京", "上海", "成都", "广州", "深圳", "重庆", "天津", "西安", "南京", "杭州"};
+    private String ages[] = {"不限", "18岁以下", "18-22岁", "23-26岁", "27-35岁", "35岁以上"};
+    private String sexs[] = {"不限", "男", "女"};
+    private String constellations[] = {"不限", "白羊座", "金牛座", "双子座", "巨蟹座", "狮子座", "处女座", "天秤座", "天蝎座", "射手座", "摩羯座", "水瓶座", "双鱼座"};
+
+    private int constellationPosition = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_institution_select);
+
+        initView();
+        initData();
     }
+
+    private void initView(){
+        mDropDownMenu = findViewById(R.id.dropDownMenu);
+
+    }
+
+    private void initData(){
+        //init city menu
+        final ListView cityView = new ListView(this);
+        cityAdapter = new GirdDropDownAdapter(this, Arrays.asList(citys));
+        cityView.setDividerHeight(0);
+        cityView.setAdapter(cityAdapter);
+
+        //init age menu
+        final ListView ageView = new ListView(this);
+        ageView.setDividerHeight(0);
+        ageAdapter = new ListDropDownAdapter(this, Arrays.asList(ages));
+        ageView.setAdapter(ageAdapter);
+
+        //init sex menu
+        final ListView sexView = new ListView(this);
+        sexView.setDividerHeight(0);
+        sexAdapter = new ListDropDownAdapter(this, Arrays.asList(sexs));
+        sexView.setAdapter(sexAdapter);
+
+        //init constellation
+        final View constellationView = LayoutInflater.from(this).inflate(R.layout.custom_layout,null);
+        GridView constellation = constellationView.findViewById(R.id.constellation);
+        constellationAdapter = new ConstellationAdapter(this, Arrays.asList(constellations));
+        constellation.setAdapter(constellationAdapter);
+        TextView ok = constellationView.findViewById(R.id.ok);
+        ok.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mDropDownMenu.setTabText(constellationPosition == 0 ? headers[3] : constellations[constellationPosition]);
+                mDropDownMenu.closeMenu();
+            }
+        });
+
+        //init popupViews
+        popupViews.add(cityView);
+        popupViews.add(ageView);
+        popupViews.add(sexView);
+        popupViews.add(constellationView);
+
+        //add item click event
+        cityView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                cityAdapter.setCheckItem(position);
+                mDropDownMenu.setTabText(position == 0 ? headers[0] : citys[position]);
+                mDropDownMenu.closeMenu();
+            }
+        });
+
+        ageView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                ageAdapter.setCheckItem(position);
+                mDropDownMenu.setTabText(position == 0 ? headers[1] : ages[position]);
+                mDropDownMenu.closeMenu();
+            }
+        });
+
+        sexView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                sexAdapter.setCheckItem(position);
+                mDropDownMenu.setTabText(position == 0 ? headers[2] : sexs[position]);
+                mDropDownMenu.closeMenu();
+            }
+        });
+
+        constellation.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                constellationAdapter.setCheckItem(position);
+                constellationPosition = position;
+            }
+        });
+
+        //init context view
+        TextView contentView = new TextView(this);
+        contentView.setLayoutParams(new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT));
+        contentView.setText("内容显示区域");
+        contentView.setGravity(Gravity.CENTER);
+        contentView.setTextSize(TypedValue.COMPLEX_UNIT_SP, 20);
+
+        //init dropdownview
+        //headers是标题头，popupview是下拉具体内容，contentView是布局
+        mDropDownMenu.setDropDownMenu(Arrays.asList(headers), popupViews, contentView);
+    }
+
+
 }
--- app/src/main/java/com/example/olderhouse/ui/fragment/HomeFragment.java
+++ app/src/main/java/com/example/olderhouse/ui/fragment/HomeFragment.java
@@ -1,12 +1,15 @@
 package com.example.olderhouse.ui.fragment;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
 
 import com.example.olderhouse.R;
+import com.example.olderhouse.ui.activity.InstitutionSelectActivity;
 import com.example.olderhouse.ui.widgets.BannerLoader;
 import com.youth.banner.Banner;
 
@@ -15,22 +18,27 @@ import java.util.List;
 
 import static com.example.olderhouse.ui.utils.ImageUtil.imageUrls;
 
-public class HomeFragment extends Fragment {
+public class HomeFragment extends Fragment implements View.OnClickListener {
 
     private Banner banner;
+    private ImageButton institution_more;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_home, container, false);
 
         initView(v);
+
         initData();
+
+        initListener();
 
         return v;
     }
 
     private void initView(View root){
         banner = root.findViewById(R.id.banner);
+        institution_more = root.findViewById(R.id.institution_more);
     }
 
     private void initData(){
@@ -43,4 +51,20 @@ public class HomeFragment extends Fragment {
         banner.start();
     }
 
+    private void initListener(){
+        institution_more.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        Intent intent = null;
+        switch(v.getId()){
+            case R.id.institution_more:
+                intent = new Intent(getContext(),InstitutionSelectActivity.class);
+                startActivity(intent);
+                break;
+            default:
+                break;
+        }
+    }
 }
